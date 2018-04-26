579398d1d6a707e07634173113efe8dab8f2eacf
Christan Fergus <cfergus@zoll.com>
2018-04-26 15:40:41 -0600

    test work done to PTT section

---
+++ assets/scripts/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  delimiters: ["((", "))"],
+  el: '#app',
+  data: {
+    names: [
+      {"first_name":"Woodman","last_name":"Gimeno"},
+      {"first_name":"Lee","last_name":"Glave"},
+      {"first_name":"Cord","last_name":"Sonnenschein"},
+      {"first_name":"Penelopa","last_name":"Fiddymont"},
+      {"first_name":"Rodger","last_name":"McKeowon"},
+      {"first_name":"Rolfe","last_name":"Edmunds"},
+      {"first_name":"Anni","last_name":"Collishaw"},
+      {"first_name":"Glad","last_name":"Fairfull"},
+      {"first_name":"Crystie","last_name":"Burch"},
+      {"first_name":"Antoinette","last_name":"Hazzard"}
+    ]
+  }
+})
